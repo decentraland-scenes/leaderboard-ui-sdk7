3fce6105adb9c70eb9a5f54873569672f54d30c4
nobodysGitHub <91459536+nobodysGitHub@users.noreply.github.com>
2023-02-17 10:40:50 +0100

    screenshot

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,10 +1,18 @@
+import { Color4 } from '@dcl/sdk/math'
 import ReactEcs, { ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
 import { createHudLeaderboardTable } from './uiHudLeaderboard'
 import { createModalLeaderboardTable } from './uiModalLeaderboard'
 
 
 const uiComponent = () => (
-  <UiEntity>
+  <UiEntity
+    uiTransform={{ 
+      width: '100%',
+      height: '100%',
+      maxWidth: '100%',
+      maxHeight: '100%'
+    }}
+  >
     <UiEntity>{createHudLeaderboardTable()}</UiEntity>
     <UiEntity>{createModalLeaderboardTable()}</UiEntity>
     
